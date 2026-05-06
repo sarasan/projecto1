e135508c1e4da10e88bf5c04bada1ed83fbde195
sara <sarahkungfu32@gmail.com>
2026-05-06 18:53:19 +0100

    up3

--- demo/src/main/java/com/Dados.java
+++ demo/src/main/java/com/Dados.java
@@ -9,12 +9,63 @@ public class Dados {
 
 private ArrayList<Viaturas> ListaViaturas = new ArrayList<>();
 
+  public boolean ViaturaJaExiste(String Matricula){
+        for(Viaturas x:ListaViaturas){
+            if(x.getMatricula()==Matricula){
+                return(true);             
+            }
+        }
+        return(false);
+    }
 
    public void AdicionarViaturas(Viaturas V) {
         ListaViaturas.add(V);
         JOptionPane.showMessageDialog(null, "Viatura adicionada.",
                 "Adicionar Viatura", JOptionPane.INFORMATION_MESSAGE);
     }
+
+
+     public void EliminarViatura(String Matricula) {
+        for (int i = 0; i < ListaViaturas.size(); i++) {
+            Viaturas V = ListaViaturas.get(i);
+            if (V.getMatricula() == Matricula) {
+                ListaViaturas.remove(i);
+                JOptionPane.showMessageDialog(null, "Viatura eliminada",
+                        "Eliminar viatura", JOptionPane.INFORMATION_MESSAGE);
+                return;
+            }
+        }
+        JOptionPane.showMessageDialog(null, "Matricula de viatura inexistente",
+                "Eliminar viatura", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+
+    public void ListagemViaturas() {
+        if(ListaViaturas.isEmpty()){
+            JOptionPane.showMessageDialog(null, "Não existem viaturas",
+                "Listagem de Viaturas", JOptionPane.INFORMATION_MESSAGE); 
+                return;           
+        }
+        String Saida = "";
+        for (int i = 0; i < ListaViaturas.size(); i++) {
+            Viaturas V = ListaViaturas.get(i);
+            Saida += "\nMarca:" + V.getMarca() + " | \nMatricula:" + V.getMatricula() +
+                    " | \nCombustivel:" + V.getCombustivel() + " | \nKilometros actuais:" + 
+                    V.getKilometrosAtuais() + ;
+        }
+        JOptionPane.showMessageDialog(null, Saida,
+                "Listagem de Alunos", JOptionPane.INFORMATION_MESSAGE);
+}}
+/* private String Marca;
+
+    private String Matricula;
+
+   private String Combustivel;
+
+    private int KilometrosAtuais;
+
+    private float Preco; */
+
 
     /*2 - Crie uma classe Dados por forma a armazenar a informação:
 ▪ Possua um método para adicionar as viaturas ao array. Não é permitido inserir
@@ -28,6 +79,6 @@ campo Preco.
 criados anteriormente recorrendo às caixas de diálogo como no exemplo mostrado na
 aula. */
     
-}
+
 
 
